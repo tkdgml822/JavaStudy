ea145cb1ff6bf0afb5c5e10efe6b9644e061f0aa
한상희 <tkdgml822@gmail.com>
2024-01-02 20:21:05 +0900

    "age sort chap20.num2 20_1"

--- src/ch20/num2/chap20_1.java
+++ src/ch20/num2/chap20_1.java
@@ -1,4 +1,37 @@
 package ch20.num2;
 
+import java.util.Arrays;
+
+class Person implements Comparable<Person>{
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return (o.age - this.age);
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + age;
+    }
+
+}
 public class chap20_1 {
+    public static void main(String[] args) {
+        Person[] ar = new Person[3];
+        ar[0] = new Person("Lee", 29);
+        ar[1] = new Person("Goo", 15);
+        ar[2] = new Person("Soo", 37);
+
+        Arrays.sort(ar);
+        for (Person a : ar) {
+            System.out.println(a);
+        }
+    }
 }
