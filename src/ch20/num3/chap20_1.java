cfa9fb3eda77ced5253b03cd9fc672833379b22e
한상희 <tkdgml822@gmail.com>
2024-01-02 21:49:25 +0900

    "age sort chap20.num3 20_1"

---
+++ src/ch20/num3/chap20_1.java
@@ -0,0 +1,39 @@
+package ch20.num3;
+
+import ch20.num2.Person;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class MyPerson implements Comparable<MyPerson>{
+    String name;
+    int age;
+    public MyPerson(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(MyPerson o) {
+        return this.name.compareTo(o.name);
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + age;
+    }
+}
+class chap20_1 {
+    public static void main(String[] args) {
+        MyPerson[] ar = new MyPerson[3];
+
+        ar[0] = new MyPerson("Lee", 29);
+        ar[1] = new MyPerson("Goo", 15);
+        ar[2] = new MyPerson("Soo", 37);
+
+        Arrays.sort(ar);
+
+        int idx = Arrays.binarySearch(ar, new MyPerson("Goo", 15));
+        System.out.println(ar[idx]);
+    }
+}
