0b54f9f246d1c513442eec845d02caf0799ed848
한상희 <tkdgml822@gmail.com>
2024-01-05 03:59:53 +0900

    챕터22-1 문제 풀이

--- src/ch22/chap22_1_1.java
+++ src/ch22/chap22_1_1.java
@@ -1,20 +1,8 @@
 package ch22;
 
-class Box<T> {
-    private T ob;
-
-    public void set(T o){
-        ob = o;
-    }
-
-    public T get() {
-        return ob;
-    }
-}
-
 public class chap22_1_1 {
-    public static void addBox(Box<? extends Integer> b1, Box<? extends Integer> b2, Box<? super Integer> b3) {
-        b3.set(b1.get() + b2.get());
+    public static void addBox(Box<? super Integer> result, Box<? extends Integer> b1, Box<? extends Integer> b2) {
+        result.set(b1.get() + b2.get());
     }
 
     public static void main(String[] args) {
@@ -24,7 +12,7 @@ public class chap22_1_1 {
         box2.set(37);
         Box<Integer> result = new Box<>();
         result.set(0);
-        addBox(box1, box2, result);
+        addBox(result , box1, box2);
         System.out.println(result.get());
     }
 }
