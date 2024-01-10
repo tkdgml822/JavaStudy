66e13ef49cb207a9f57e8a7af7bb6f477ead6490
한상희 <tkdgml822@gmail.com>
2024-01-10 20:27:29 +0900

    chap23_2 문제

---
+++ src/ch23/chap23_2.java
@@ -0,0 +1,22 @@
+package ch23;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+class ComparatorIntegerDemo implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2.intValue() - o1.intValue();
+    }
+}
+public class chap23_2 {
+    public static void main(String[] args) {
+        TreeSet<Integer> tr = new TreeSet<>(new ComparatorIntegerDemo());
+        tr.add(30);
+        tr.add(10);
+        tr.add(20);
+        System.out.println(tr);
+    }
+
+
+}
