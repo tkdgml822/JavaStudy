738a9b13dd46a4d68131276faa4866bca2df1bda
한상희 <tkdgml822@gmail.com>
2024-03-05 17:51:12 +0900

    문제 28_2_1문제 복습

--- src/ch28/chap28_2_1.java
+++ src/ch28/chap28_2_1.java
@@ -1,28 +1,38 @@
 package ch28;
 
+import ch22.Box;
+
 import java.util.function.BiFunction;
 
-class Box<T, U> {
+class Box1<T, U> {
     private T id;
     private U con;
 
-    public Box(T i, U c) {
+    public Box1(T i, U c) {
         id = i;
         con = c;
     }
-
     public void showIt() {
-        System.out.println("id :" + id + ", con: " + con);
+        System.out.println("ID: " + id + ", " + "Contents" + con);
     }
 }
 
-class chap28_2 {
+class chap28_2_1 {
     public static void main(String[] args) {
-        BiFunction<Integer, String, Box<Integer, String>> bi = (i, s) -> new Box<>(i, s);
 
-        int id = 0;
-        Box<Integer, String> b1 = bi.apply(++id, "robot");
-        Box<Integer, String> b2 = bi.apply(++id, "cake");
+        // BiFunction 함수형 인터페이스는 BiFunction<T, U, R>이 있으며 R을 반환한다.
+        // 즉 이 코드는 apply 함수형에 i랑 s를 저장하겠다는 뜻이다.
+
+        /*
+        Box1<Integer, String> apply {
+            return new Box<>(i, s)
+        }
+        */
+
+        BiFunction<Integer, String, Box1<Integer, String>> bf = (i, s) -> new Box1<>(i, s);
+
+        Box1<Integer, String> b1 = bf.apply(1, "Toy");
+        Box1<Integer, String> b2 = bf.apply(2, "Robot");
 
         b1.showIt();
         b2.showIt();
