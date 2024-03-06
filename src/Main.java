2184290c969cacadfc7232546ebf0a3bbb599e3b
한상희 <tkdgml822@gmail.com>
2024-03-06 12:40:03 +0900

    ㅐ거ㅐ쟈더걎댜

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-
 class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
-
+        System.out.println(12);
     }
 }
