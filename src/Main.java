ca6e428e5c8f8e2687811fc626b0b69b7f996d13
한상희 <tkdgml822@gmail.com>
2024-03-06 12:57:22 +0900

    Main 변경

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+
 class Main {
     public static void main(String[] args) {
-        System.out.println(12);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+
     }
 }
