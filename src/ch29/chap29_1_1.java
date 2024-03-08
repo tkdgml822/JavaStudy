ecc2592666fef90b44c9a868954eab798998ec05
한상희 <tkdgml822@gmail.com>
2024-03-08 13:50:56 +0900

    문제 29_1_1 메소드 참조 방식으로 변경

--- src/ch29/chap29_1_1.java
+++ src/ch29/chap29_1_1.java
@@ -7,7 +7,9 @@ public class chap29_1_1 {
     public static void main(String[] args) {
         List<Box<String>> ls  = Arrays.asList(new Box<>("Robot"), new Box<>("Simple"));
         ls.stream()
-        .map(s -> s.get())
+        .map(Box::get)
         .forEach(System.out::println);
+
+        // Box::get 메소드 참조로 변경 Box 클래스에 접근해서 -> get 메소드로 접근
     }
 }
